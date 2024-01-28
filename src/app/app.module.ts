a0d6e42b583578cc264ed63b38cfda6377ea028b
rania <badrirania35@gmail.com>
2024-01-28 21:52:37 +0100

    Premier commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MemberComponent } from './member/member.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MemberComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/member/member.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member',
+  templateUrl: './member.component.html',
+  styleUrls: ['./member.component.css']
+})
+export class MemberComponent {
+
+  nom='glid2';
+  
+
+}
